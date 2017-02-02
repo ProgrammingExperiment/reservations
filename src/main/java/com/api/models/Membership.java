fa3ac87e67a90f2416b26c34a3d171b66b025725
shawn haler <shanwhaler@yahoo.com>
2017-02-02 06:40:25 -0700

    membership model and controller

---
+++ src/main/java/com/api/models/Membership.java
@@ -0,0 +1,23 @@
+package com.api.models;
+
+public class Membership {
+	private long Id;
+	private String past;
+	private String future;
+	
+	public Membership(long id,String past ,String future){
+		this.Id = id;
+		this.past = past;
+		this.future = future;
+	}
+	
+	public long getId(){
+		return this.Id;
+	}
+	public String getPast(){
+		return this.past;
+	}
+	public String getFuture(){
+		return this.future;
+	}
+}
